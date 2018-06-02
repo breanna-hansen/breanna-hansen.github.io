fd834ce88db0fe242ad7df0df149fe9b4cef4078
breanna-hansen <breannahansen@gmail.com>
2018-06-01 23:30:02 -0600

    new Franklin images

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,6 +1,10 @@
-  var d = new Date();
+  function currentDate() {
+    var d = new Date();
   var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var n = weekday[d.getDay()];
   var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var m = month[d.getMonth()];
-  document.getElementById("currentdate").innerHTML = n + ", " + d.getDate() + " " + m + " " + d.getFullYear();
+   return (n + ", " + d.getDate() + " " + m + " " + d.getFullYear());
+  }
+
+  document.getElementById("currentdate").innerHTML = currentDate();
