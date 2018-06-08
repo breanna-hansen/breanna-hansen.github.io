a1a0df9fe8094f61df07ad065d3b12412e488640
breanna-hansen <breannahansen@gmail.com>
2018-06-08 16:11:24 -0600

    nav

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -15,7 +15,7 @@ function calculateWindChill()
 function windChill(highT, lowT, speed)
 {
   let avg = average(highT, lowT);
-  let windChillResult = 35.74 + (0.6215 * avg) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * avg * Math.pow(speed, 0.16));
+  let windChillResult = 35.74 + 0.6215 * avg - 35.75 * Math.pow(speed, 0.16) + 0.4275 * avg * Math.pow(speed, 0.16);
   let result = Math.round(windChillResult);
   return result;
 }
