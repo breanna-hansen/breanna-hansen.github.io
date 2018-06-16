a4e0fbaaed568dbc4e8a29f793d30f870e94e4f3
breanna-hansen <breannahansen@gmail.com>
2018-06-16 02:31:35 -0600

    JSON home page

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,12 +1,138 @@
-let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', requestURL);
-weatherRequest.send();
-weatherRequest.onload =  function () {
-  let weatherData = JSON.parse(weatherRequest.responseText);
-  /*document.getElementById('towns').innerHTML = weatherData.towns[0]['name']; */
-  document.getElementById('events').innerHTML = weatherData.towns[0]['events'];
+function GetData(page){
+  let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+  let weatherRequest = new XMLHttpRequest();
+  weatherRequest.open('GET', requestURL);
+  weatherRequest.send();
+  weatherRequest.onload = function () {
+      let weatherData = JSON.parse(weatherRequest.responseText);
+    switch(page) {
+      case 'homePage':
+        homePage(weatherData);
+        break;
+      case 'franklin':
+        franklinData(weatherData);
+        break;
+      case 'springField':
+        springfieldData(weatherData);
+        break;
+      default:
+        greenvilleData(weatherData);
+    }
   }
+}
+
+function homePage(data) {
+  /*document.getElementById('cityName').innerHTML = data.towns[0].name; */
+    let dataSet = data.towns;
+    let allData = buildTable(dataSet);
+      document.getElementById('Franklin').innerHTML = buildTable(data.towns[0]);
+      document.getElementById('Springfield').innerHTML = buildTable(data.towns[1]);
+      document.getElementById('Greenville').innerHTML = buildTable(data.towns[3]);
+    }
+
+  //document.getElementById('stats').innerHTML = allData;
+   /* "<table>" +
+    "<tbody>" +
+    "<tr>" +
+    "<td>Motto: </td>" +
+    "<td>" + data.towns[0].motto + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Year founded: </td>" +
+    "<td>" + data.towns[0].yearFounded + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Population: </td>" +
+    "<td>" + data.towns[0].currentPopulation + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Annual rainfall: </td>" +
+    "<td>" + data.towns[0].averageRainfall +  "</td>" +
+    "</tr>" +
+    "</tbody>" +
+    "</table>"; */
+
+
+
+
+   /* "Motto: " + data.towns[0].motto + "<br>" + "Year Founded: " + data.towns[0].yearFounded + "<br>" + "Population: " + data.towns[0].currentPopulation + "<br>" + "Average rainfall: " + data.towns[0].averageRainfall; */
+/*  document.getElementById('motto').innerHTML = data.towns[0].motto;
+  document.getElementById('year').innerHTML = data.towns[0].yearFounded;
+  document.getElementById('population').innerHTML = data.towns[0].currentPopulation;
+  document.getElementById('rainfall').innerHTML = data.towns[0].averageRainfall;*/
+
+function buildTable(tableData){
+  allData = "<table>" +
+    "<tbody>" +
+    "<tr colspan='2'>" +
+    "<td><h3>" + tableData.name +
+    "</h3></td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Motto: </td>" +
+    "<td>" + tableData.motto + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Year founded: </td>" +
+    "<td>" + tableData.yearFounded + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Population: </td>" +
+    "<td>" + tableData.currentPopulation + "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>Annual rainfall: </td>" +
+    "<td>" + tableData.averageRainfall +  "</td>" +
+    "</tr>" +
+    "</tbody>" +
+    "</table>";
+  return allData;
+}
+
+
+function franklinData(data) {
+  let event = data.towns[0].events;
+  let date = "";
+  let x;
+  for (x in event) {
+    date += event[x] + "<br>";
+  }
+  document.getElementById('events').innerHTML = date;
+  //document.getElementById('motto').innerHTML = data.towns[0].motto;
+}
+
+function springfieldData(data) {
+
+}
+
+function greenvilleData(data) {
+
+}
+
+/*
+  functions setFranklin(){
+    let data = getData();
+  }
+  function setIndex(){
+
+  }
+document.getElementById('towns').innerHTML = weatherData.towns[0]['name'];
+let motto = weatherData.towns[0]['motto'];
+
+  let event = weatherData.towns[0]['events'];
+  let date = "";
+  var x;
+  for (x in event) {
+    date += event[x] + "<br>";
+  }
+
+  document.getElementById('events').innerHTML = date;
+  document.getElementById('motto').innerHTML = motto; */
+
+  /*
+  let motto = weatherData.towns[0][motto];
+  document.getElementById9('motto').innerHTML = motto;
+  */
 
 /*
 {"towns" : [ {"name": "Franklin", "motto": "Where you will grow!", "yearFounded": 1788, "currentPopulation": 30458, "averageRainfall": 21,"events" : [
@@ -19,3 +145,4 @@ weatherRequest.onload =  function () {
         "January 8: Spring into Winter", "April 10-20: Celebration of Life", "July 31-Aug 15: Dog Days of Summer Festival"] }
           ] }
           */
+
