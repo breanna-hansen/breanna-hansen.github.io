a28cbaac949c4dfc635be0c76e1ac7e2f66b52f7
breanna-hansen <breannahansen@gmail.com>
2018-06-19 20:06:47 -0600

    https

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,4 +1,4 @@
-let requestURL = 'http://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=ef96bb82b8c215e4ec5411a85a1e3c8e&units=imperial';
+let requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=ef96bb82b8c215e4ec5411a85a1e3c8e&units=imperial';
   let weatherRequest = new XMLHttpRequest();
   weatherRequest.open('GET', requestURL);
   weatherRequest.send();
