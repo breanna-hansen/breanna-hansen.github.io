5a0d3222bb0652f7c504bd5e6bbb30a901ffcbaf
breanna-hansen <breannahansen@gmail.com>
2018-06-22 20:17:30 -0600

    fixed stats centering

--- assignments/lesson-9/scripts/towndata.js
+++ assignments/lesson-9/scripts/towndata.js
@@ -12,7 +12,7 @@ function GetData(page){
       case 'franklin':
         franklinData(weatherData);
         break;
-      case 'springField':
+      case 'springfield':
         springfieldData(weatherData);
         break;
       default:
@@ -22,12 +22,11 @@ function GetData(page){
 }
 
 function homePage(data) {
-  /*document.getElementById('cityName').innerHTML = data.towns[0].name; */
     let dataSet = data.towns;
     let allData = buildTable(dataSet);
-      document.getElementById('franklinCity').innerHTML = buildTable(data.towns[0]);
-      document.getElementById('Springfield').innerHTML = buildTable(data.towns[1]);
-      document.getElementById('Greenville').innerHTML = buildTable(data.towns[3]);
+    document.getElementById('franklinCity').innerHTML = buildTable(data.towns[0]);
+    document.getElementById('Springfield').innerHTML = buildTable(data.towns[1]);
+    document.getElementById('Greenville').innerHTML = buildTable(data.towns[3]);
     }
 
 function buildTable(tableData){
@@ -78,15 +77,4 @@ function greenvilleData(data) {
 
 }
 
-/*
-{"towns" : [ {"name": "Franklin", "motto": "Where you will grow!", "yearFounded": 1788, "currentPopulation": 30458, "averageRainfall": 21,"events" : [
-        "March 4: March to the Drum of Donuts", "September 5 - 11: Founder Days", "December 1 - 26: Christmas in the Heart"] },
-{"name": "Greenville", "motto": "Green is our way of life.", "yearFounded": 1805, "currentPopulation": 33458, "averageRainfall": 25, "events" : [
-        "February 10-12: Greenbration", "May 8 - 18: Greenville Founder Days", "June 20: Verde and Valiant Day", "November 15-16: Greensome Gathering"] },
-{"name": "Placerton", "motto": "Positive Placement in Placerton.", "yearFounded": 1946, "currentPopulation": 512, "averageRainfall": 39, "events" : [
-        "July 4: A Blaze of Glory", "October 20: Fall through Fall"] },
-{ "name": "Springfield", "motto": "Where everyone is lifted.", "yearFounded": 1826, "currentPopulation": 17852, "averageRainfall": 17, "events" : [
-        "January 8: Spring into Winter", "April 10-20: Celebration of Life", "July 31-Aug 15: Dog Days of Summer Festival"] }
-          ] }
-          */
 
