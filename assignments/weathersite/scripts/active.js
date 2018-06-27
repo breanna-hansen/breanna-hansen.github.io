9ed54287192a909bc65b2d18c3b5383edffefbf0
breanna-hansen <breannahansen@gmail.com>
2018-06-26 20:38:08 -0600

    javascript for nav

---
+++ assignments/weathersite/scripts/active.js
@@ -0,0 +1,15 @@
+function activeStyle() {
+  switch(page) {
+    case 'homePage':
+      break;
+    case 'franklin':
+      franklinData(weatherData);
+      break;
+    case 'springfield':
+      springfieldData(weatherData);
+      break;
+    case 'stormcenter':
+    case 'gallery':
+    case 'greenville':
+    default:
+}
